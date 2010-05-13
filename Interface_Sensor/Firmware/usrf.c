8d0fcb6bc7adb2d1171274ea985deb09956277e5
Olivier BICHLER <olivier.bichler@crans.org>
2010-05-13 11:18:38 +0200

    Changed I2C protocol

--- Interface_Sensor/Firmware/usrf.c
+++ Interface_Sensor/Firmware/usrf.c
@@ -27,12 +27,17 @@ WORD usrfGet(BYTE id) {
     OpenI2C(MASTER, SLEW_OFF);
     StartI2C();
     IdleI2C();
-    WriteI2C((id << 1) & 0b1);       // 7-bit address + read
-    
+    WriteI2C(id << 1);              // 7-bit address + write
+    WriteI2C('g');
+
+    RestartI2C();
+    IdleI2C();
+    WriteI2C((id << 1) & 0b1);      // 7-bit address + read
+
     SSPCON2bits.RCEN = 1;
     timeOut = TRUE;
 
-    for (count = 0; count < 10; count++) {
+    for (count = 0; count < 1000; count++) {
         if (DataRdyI2C()) {
             value.byte.HB = SSPBUF;
             timeOut = FALSE;
@@ -40,13 +45,13 @@ WORD usrfGet(BYTE id) {
         }
     }
 
-    AckI2C();
-
     if (!timeOut) {
+        AckI2C();
+
         SSPCON2bits.RCEN = 1;
         timeOut = TRUE;
     
-        for (count = 0; count < 10; count++) {
+        for (count = 0; count < 1000; count++) {
             if (DataRdyI2C()) {
                 value.byte.LB = SSPBUF;
                 timeOut = FALSE;
